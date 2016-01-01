8f4f9ab17ddb9639460094313c1053dd26dfd484
Rohan Tripathi <rohantripathi26@gmail.com>
2016-01-01 15:06:15 -0800

    commit-2

--- mastermindGUI/src/project6/Game.java
+++ mastermindGUI/src/project6/Game.java
@@ -6,7 +6,7 @@ package project6;
 /**
  * Class able of holding String of pegs and the number of black and white pegs used.
  * 
- * @author Rohan T. and Bryan V.
+ * @author Rohan Tripathi
  *
  */
 public class Game{
